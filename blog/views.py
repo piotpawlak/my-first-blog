c2040a329296dc90a94978094d160d1be80f2a13
pepito <pawlaczky@gmail.com>
2020-09-06 15:37:33 +0200

    Powrot

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
+    posts = Post.objects.filer(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
 def strona_glowna(request):
     imie = "Piotr"
