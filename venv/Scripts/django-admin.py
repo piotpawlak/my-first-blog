f0cb78707d2e981cd49f4fdc6bf72e080607d7e5
piotpawlak <1piotr.pawlak@gmail.com>
2020-09-15 17:36:30 +0200

    base+post_details_list

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ppawlak\pycharmprojects\my-first-blog\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
