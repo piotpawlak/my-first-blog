4f6639645b833bf6e3a450e8a65fe9aafd5b9938
piotpawlak <1piotr.pawlak@gmail.com>
2020-09-15 18:21:09 +0200

    hmm

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views
+
 
 urlpatterns = [
     path('', include('blog.urls')),
-    path('data/', views.data),
     path('admin/', admin.site.urls),
 ]
